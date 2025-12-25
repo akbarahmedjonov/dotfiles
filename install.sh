4ac377b3e73c1226d3d5a3a41c83579aaf55d13a
akbarahmedjonov <teamender1991@gmail.com>
2025-12-25 09:51:17 +0500

    Clipboard History Support

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-yay -S stow noctalia-shell papirus-icon-theme adw-gtk-theme cava btop vscodium vscodium-marketplace fish hyprland hypridle hyprsunset wlsunset kitty neovim vim nwg-look yazi ttf-jetbrains-mono-nerd
+yay -S --needed stow noctalia-shell papirus-icon-theme adw-gtk-theme cava btop vscodium vscodium-marketplace fish hyprland hypridle hyprsunset wlsunset kitty neovim vim nwg-look yazi ttf-jetbrains-mono-nerd imv mpv cliphist
 rm -rf ~/.config/btop ~/.config/cava ~/.config/Code ~/.config/fish ~/.config/gtk-3.0/ ~/.config/gtk-4.0/ ~/.config/htop ~/.config/hypr ~/.config/kitty ~/.config/noctalia/ ~/.config/nvim/ ~/.config/nwg-look/ ~/.config/qt5ct/ ~/.config/qt6ct/ ~/.config/VSCodium/ ~/.config/yazi/
 stow .
